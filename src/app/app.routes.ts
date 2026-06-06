bf1c1a533958f88fc0f55d6bf3b4fffea03b8538
snandip9 <siri.nandipaty2@mail.dcu.ie>
2026-06-06 22:42:53 +0100

    Changed the css and html of the homepage entirely, added necessary images, created the starting of the routes for the navigation outside of the homepage but none of them are properly connected yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import {CV} from '/cv/cv';
+import {Contact} from '/contact/contact';
+import {Projects} from '/projects/projects';
+import {Blog} from '/blog/blog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+	{
+		path: 'cv',
+		component: CV
+	},
+
+	{
+		path: 'contact',
+		component: Contact
+	},
+
+	{
+		path: 'projects',
+		component: Projects
+	},
+
+	{
+		path: 'blog',
+		component: Blog
+	}
+
+
+];
---
+++ src/app/cv/cv.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv',
+  imports: [],
+  templateUrl: './cv.html',
+  styleUrl: './cv.css',
+})
+export class CV {}
