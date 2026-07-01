d2d6e7dbc7bff9049333caccab2eed7ddbaf139e
snandip9 <siri.nandipaty2@mail.dcu.ie>
2026-07-01 22:35:58 +0100

    moved files again

--- app/coolstuff/coolstuff.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-coolstuff',
-  imports: [],
-  templateUrl: './coolstuff.html',
-  styleUrl: './coolstuff.css',
-})
-export class Coolstuff {}
--- app/cv/cv.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cv',
-  imports: [],
-  templateUrl: './cv.html',
-  styleUrl: './cv.css',
-})
-export class CV {}
