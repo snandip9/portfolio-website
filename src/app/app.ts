57a060aad216d34f0bb2bd5c046ca625886c1481
snandip9 <siri.nandipaty2@mail.dcu.ie>
2026-07-01 22:06:53 +0100

    fixed the cv page and also added pgp key copy and paste function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink} from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-start',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('portfolio');
+
 }
